b637d59c6588f75d78cc3d7d71bcbbd273382057
ConorL11 <conorlamb11@gmail.com>
2023-12-11 11:53:24 -0700

    Responsive Design Adjustment

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
             </li>
           </ul>
         </header>
-        <main id="content" className="pt-24 lg:w-2/3 lg:py-24">
+        <main id="content" className="lg:pt-24 lg:w-2/3 pt-8 lg:py-24">
           <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
             <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-stone-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
               <h2 className="text-sm font-bold uppercase tracking-widest text-stone-200 lg:sr-only">About</h2>
