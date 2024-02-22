bc3ffa5cf158eb97e152696dfa5fe5094ca259e3
ConorL11 <74984940+ConorL11@users.noreply.github.com>
2024-02-22 09:55:48 -0700

    Portfolio Update

--- src/App.js
+++ src/App.js
@@ -71,7 +71,44 @@ function App() {
                     <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-stone-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg">
                     </div>
                     <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-stone-500 sm:col-span-2">
-                      2021 - 2023
+                      2024 - Current
+                    </header>
+                    <div className="z-10 sm:col-span-6">
+                      <h3 className="font-medium leading-snug text-stone-200 mb-4">
+                        <div>
+                          <a className="inline-flex items-baseline font-medium leading-tight text-stone-200 hover:text-yellow-300 focus-visible:text-yellow-300 group/link text-base" target="_blank" href="https://configureone.com" rel="noreferrer">
+                            <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                              <span className="inline-block">
+                                 Revalize (Configure One Cloud)
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-transtone-y-1 group-hover/link:transtone-x-1 group-focus-visible/link:-transtone-y-1 group-focus-visible/link:transtone-x-1 motion-reduce:transition-none ml-1 transtone-y-px group-hover:translate-x-1 group-hover:-translate-y-1">
+                                  <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path>
+                                </svg>
+                              </span>
+                          </a>
+                        </div>
+                      </h3>
+                      <h4 className="font-medium leading-snug text-stone-300">Senior Implementation Specialist (Contract / Part Time)</h4>
+                      <p className="mt-2 text-sm leading-normal">Configure One Cloud CPQ SME working with Professional Services</p>
+                      <ul className="mt-2 flex flex-wrap mb-6">
+                        <li className="mr-1.5 mt-2">
+                          <SkillBubble>CPQ</SkillBubble>
+                        </li>
+                        <li className="mr-1.5 mt-2">
+                          <SkillBubble>JQuery</SkillBubble>
+                        </li>
+                        <li className="mr-1.5 mt-2">
+                          <SkillBubble>Technical Solution Design</SkillBubble>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+                <li className="mb-12">
+                  <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                    <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-stone-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg">
+                    </div>
+                    <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-stone-500 sm:col-span-2">
+                      2021 - 2024
                     </header>
                     <div className="z-10 sm:col-span-6">
                       <h3 className="font-medium leading-snug text-stone-200 mb-4">
